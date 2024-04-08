d5bcbcac20528be57b4ee51516c475baf19f736a
suadou <sergio.suarezdou@uni.lu>
2024-04-08 16:41:50 +0200

    Updating neighborlist.py. Adding comments and change names of variables to make it more clear.

--- glp/neighborlist.py
+++ glp/neighborlist.py
@@ -9,9 +9,9 @@ This file implements this in two ways:
             candidates into a fixed number of pairs. Once that number is
             decided, this procedure is jittable. The initial allocation is not.
 
-- Cell-list: We first divide the simulation cell into a grid of cells, and 
-                assign each atom to a cell. Then, for each atom, we only consider
-                the atoms in the same cell and its neighbors. This is jittable. 
+- Cell-list: We first divide the simulation cell into a grid of bins, and 
+                assign each atom to a bin. Then, for each atom, we only consider
+                the atoms in the same bin and its neighbors. This is jittable. 
                 The initial allocation is not.
 
 The cell-list implementation is more efficient for large systems, but it is
@@ -42,7 +42,7 @@ Neighbors = namedtuple(
 )
 
 CellList = namedtuple(
-    "CellList", ("id", "reallocate", "capacity", "size", "cells_per_side")
+    "CellList", ("id", "reallocate", "capacity", "size", "bins_per_side")
 )
 
 def neighbor_list(system, cutoff, skin, capacity_multiplier=1.25):
@@ -145,6 +145,7 @@ def quadratic_neighbor_list(cell, cutoff, skin, capacity_multiplier=1.25, use_ce
             new_cell = stop_gradient(new_cell)
 
         N = positions.shape[0]
+        # Check if we are using cell list
         cl = cl_allocate(positions)  if cl_allocate is not None else None
         centers, others, sq_distances, mask, hits = get_neighbors(
             positions,
@@ -173,6 +174,7 @@ def quadratic_neighbor_list(cell, cutoff, skin, capacity_multiplier=1.25, use_ce
         dim = positions.shape[1]
         size = neighbors.centers.shape[0]
         def update(positions, cell, padding_mask, cl=neighbors.cell_list):
+            # Check if we are using cell list
             cl = cl_update(positions, cl, new_cell)  if cl_update is not None else None
             centers, others, sq_distances, mask, hits = get_neighbors(
                 positions,
@@ -210,21 +212,24 @@ def candidates_fn(n):
     others = jnp.reshape(square, (-1,))
     return centers, others
 
-def cell_list_candidate_fn(cell_id, N, dim):
-    idx = cell_id
-    cell_idx = [idx] * (dim**3)
-    for i, dindex in enumerate(neighboring_cells(dim)):
-        cell_idx[i] = shift_array(idx, dindex)
+def cell_list_candidate_fn(bin_id, N, dim):
+    """Get the candidates for the cell list neighbor list. It is implemented following the jax-md implementation."""
+    idx = bin_id
+    bin_idx = [idx] * (dim**3)
+    # Get the neighboring bins from the current bin
+    for i, dindex in enumerate(neighboring_bins(dim)):
+        bin_idx[i] = shift_array(idx, dindex)
 
-    cell_idx = jnp.concatenate(cell_idx, axis=-2)
-    cell_idx = cell_idx[..., jnp.newaxis, :, :]
-    cell_idx = jnp.broadcast_to(cell_idx, idx.shape[:-1] + cell_idx.shape[-2:])
-    def copy_values_from_cell(value, cell_value, cell_id):
-        scatter_indices = jnp.reshape(cell_id, (-1,))
-        cell_value = jnp.reshape(cell_value, (-1,) + cell_value.shape[-2:])
-        return value.at[scatter_indices].set(cell_value)
-    neighbor_idx = jnp.zeros((N + 1,) + cell_idx.shape[-2:], jnp.int32)
-    neighbor_idx = copy_values_from_cell(neighbor_idx, cell_idx, idx)
+    bin_idx = jnp.concatenate(bin_idx, axis=-2)
+    bin_idx = bin_idx[..., jnp.newaxis, :, :]
+    bin_idx = jnp.broadcast_to(bin_idx, idx.shape[:-1] + bin_idx.shape[-2:])
+    def copy_values_from_bin(value, bin_value, bin_id):
+        scatter_indices = jnp.reshape(bin_id, (-1,))
+        bin_value = jnp.reshape(bin_value, (-1,) + bin_value.shape[-2:])
+        return value.at[scatter_indices].set(bin_value)
+    neighbor_idx = jnp.zeros((N + 1,) + bin_idx.shape[-2:], jnp.int32)
+    neighbor_idx = copy_values_from_bin(neighbor_idx, bin_idx, idx)
+    # Reshape the neighbor_idx to get the centers and others
     others = jnp.reshape(neighbor_idx[:-1, :, 0], (-1,))
     centers = jnp.repeat(jnp.arange(0, N), others.shape[0] // N)
     return centers, others
@@ -253,62 +258,65 @@ def make_squared_distance(cell):
 
 
 def cell_list(cell, cutoff, buffer_size_multiplier=1.25, bin_size_multiplier=1):
+    """Implementation of cell list neighborlist in pbc."""
 
     cell = jnp.array(cell)
     cutoff *= bin_size_multiplier
     def allocate_fn(positions, extra_capacity=0):
         # This function is not jittable, we're determining shapes
         N = positions.shape[0]
-        _, cell_size, cells_per_side, cell_count = cell_dimensions(cell, cutoff)
-        cell_capacity = estimate_cell_capacity(positions, cell, cell_size,
+        _, bin_size, bins_per_side, bin_count = bin_dimensions(cell, cutoff)
+        bin_capacity = estimate_bin_capacity(positions, cell, bin_size,
                                             buffer_size_multiplier)
-        cell_capacity += extra_capacity
+        # Computing the maximum number of atoms per bin (capacity)
+        # extra_capacity is used to increase the capacity of the bins to avoid reallocation
+        bin_capacity += extra_capacity
         
         overflow = False
-        cell_id = N * jnp.ones((cell_count * cell_capacity, 1), dtype=jnp.int32)
+        bin_id = N * jnp.ones((bin_count * bin_capacity, 1), dtype=jnp.int32)
 
-
-        hash_multipliers = compute_hash_constants(cells_per_side)
+        # Compute the hash of each particle to assign it to a bin
+        hash_multipliers = compute_hash_constants(bins_per_side)
         particle_id = iota(jnp.int32, N)
-        indices = jnp.array(jnp.floor(positions @ inverse(cell_size).T), dtype=jnp.int32)
-        # Some particles are in the edge and might have negative indices or larger than cells_per_side
-        # We need to correct them wrapping into cell per side vector
-        indices = wrap(jnp.diag(cells_per_side), indices).astype(jnp.int32)
+        indices = jnp.array(jnp.floor(positions @ inverse(bin_size).T), dtype=jnp.int32)
+        # Some particles are in the edge and might have negative indices or larger than bins_per_side
+        # We need to correct them wrapping into bin per side vector
+        indices = wrap(jnp.diag(bins_per_side), indices).astype(jnp.int32)
         hashes = jnp.sum(indices * hash_multipliers, axis=1, dtype=jnp.int32)
 
         sort_map = jnp.argsort(hashes)
         sorted_hash = hashes[sort_map]
         sorted_id = particle_id[sort_map]
 
-        sorted_cell_id = jnp.mod(iota(jnp.int32, N), cell_capacity)
-        sorted_cell_id = sorted_hash * cell_capacity + sorted_cell_id
+        sorted_bin_id = jnp.mod(iota(jnp.int32, N), bin_capacity)
+        sorted_bin_id = sorted_hash * bin_capacity + sorted_bin_id
         sorted_id = jnp.reshape(sorted_id, (N, 1))
-        cell_id = cell_id.at[sorted_cell_id].set(sorted_id)
-        cell_id = unflatten_cell_buffer(cell_id, cells_per_side)
-        occupancy = ops.segment_sum(jnp.ones_like(hashes), hashes, cell_count)
+        bin_id = bin_id.at[sorted_bin_id].set(sorted_id)
+        bin_id = unflatten_bin_buffer(bin_id, bins_per_side)
+        occupancy = ops.segment_sum(jnp.ones_like(hashes), hashes, bin_count)
         max_occupancy = jnp.max(occupancy)
-        overflow = overflow | (max_occupancy > cell_capacity)
-        return CellList(cell_id, overflow, cell_capacity, cell_size, cells_per_side)
+        overflow = overflow | (max_occupancy > bin_capacity)
+        return CellList(bin_id, overflow, bin_capacity, bin_size, bins_per_side)
     
     def update_fn(positions, old_cell_list, new_cell):
         # this is jittable,
         # CellList tells us all the shapes we need
-        # If cell size is lower than cutoff, we need to reallocate cell list
+        # If bin size is lower than cutoff, we need to reallocate cell list
         # Rellocate is not jitable, we're changing shapes
         # So, after each update we need to check if we need to reallocate
         N = positions.shape[0]
 
-        cell_size = jnp.where(old_cell_list.cells_per_side != 0, cell / old_cell_list.cells_per_side, cell)
-        max_occupancy = estimate_cell_capacity(positions, new_cell, cell_size, 1)
+        bin_size = jnp.where(old_cell_list.bins_per_side != 0, cell / old_cell_list.bins_per_side, cell)
+        max_occupancy = estimate_bin_capacity(positions, new_cell, bin_size, 1)
         # Checking if update or reallocate
-        reallocate = jnp.all(get_heights(cell_size).any() >= cutoff) & (max_occupancy <= old_cell_list.capacity)
+        reallocate = jnp.all(get_heights(bin_size).any() >= cutoff) & (max_occupancy <= old_cell_list.capacity)
 
         def update(positions, old_cell_list):
-            hash_multipliers = compute_hash_constants(old_cell_list.cells_per_side)
-            indices = jnp.array(jnp.floor(positions @ inverse(cell_size).T), dtype=jnp.int32)
-            # Some particles are in the edge and might have negative indices or larger than cells_per_side
-            # We need to correct them wrapping into cell per side vector
-            indices = wrap(jnp.diag(old_cell_list.cells_per_side), indices).astype(jnp.int32)
+            hash_multipliers = compute_hash_constants(old_cell_list.bins_per_side)
+            indices = jnp.array(jnp.floor(positions @ inverse(bin_size).T), dtype=jnp.int32)
+            # Some particles are in the edge and might have negative indices or larger than bins_per_side
+            # We need to correct them wrapping into bin per side vector
+            indices = wrap(jnp.diag(old_cell_list.bins_per_side), indices).astype(jnp.int32)
             
             hashes = jnp.sum(indices * hash_multipliers, axis=1, dtype=jnp.int32)
             particle_id = iota(jnp.int32, N)
@@ -316,68 +324,72 @@ def cell_list(cell, cutoff, buffer_size_multiplier=1.25, bin_size_multiplier=1):
             sorted_hash = hashes[sort_map]
             sorted_id = particle_id[sort_map]
 
-            sorted_cell_id = jnp.mod(iota(jnp.int32, N), old_cell_list.capacity)
-            sorted_cell_id = sorted_hash * old_cell_list.capacity + sorted_cell_id
+            sorted_bin_id = jnp.mod(iota(jnp.int32, N), old_cell_list.capacity)
+            sorted_bin_id = sorted_hash * old_cell_list.capacity + sorted_bin_id
             sorted_id = jnp.reshape(sorted_id, (N, 1))
-            cell_id = N * jnp.ones((old_cell_list.id.reshape(-1, 1).shape), dtype=jnp.int32)
-            cell_id = cell_id.at[sorted_cell_id].set(sorted_id)
+            bin_id = N * jnp.ones((old_cell_list.id.reshape(-1, 1).shape), dtype=jnp.int32)
+            bin_id = bin_id.at[sorted_bin_id].set(sorted_id)
 
-            # This is not jitable, we're changing shapes. It's a fix for the unflatten_cell_buffer.
+            # This is not jitable, we're changing shapes. It's a fix for the unflatten_bin_buffer.
             
-            cell_id = jax.pure_callback(unflatten_cell_buffer, old_cell_list.id, cell_id, old_cell_list.cells_per_side)
-            return CellList(cell_id, old_cell_list.reallocate, old_cell_list.capacity, cell_size, old_cell_list.cells_per_side)
+            bin_id = jax.pure_callback(unflatten_bin_buffer, old_cell_list.id, bin_id, old_cell_list.bins_per_side)
+            return CellList(bin_id, old_cell_list.reallocate, old_cell_list.capacity, bin_size, old_cell_list.bins_per_side)
         
-        # In case cell size is lower than cutoff, we need to reallocate 
+        # In case bin size is lower than cutoff, we need to reallocate 
         def need_reallocate(_ ,old_cell_list):
-            return CellList(old_cell_list.id, True, old_cell_list.capacity, cell_size, old_cell_list.cells_per_side)
+            return CellList(old_cell_list.id, True, old_cell_list.capacity, bin_size, old_cell_list.bins_per_side)
         return cond(reallocate, need_reallocate, update, positions, old_cell_list)
     return allocate_fn, update_fn 
 
 
-def estimate_cell_capacity(positions, cell, cell_size, buffer_size_multiplier):
-    minimum_cell_size = jnp.min(get_heights(cell_size))
-    cell_capacity = jnp.max(count_cell_filling(positions, cell, minimum_cell_size))
-    return (cell_capacity * buffer_size_multiplier).astype(jnp.int32)
+def estimate_bin_capacity(positions, cell, bin_size, buffer_size_multiplier):
+    minimum_bin_size = jnp.min(get_heights(bin_size))
+    bin_capacity = jnp.max(count_bin_filling(positions, cell, minimum_bin_size))
+    return (bin_capacity * buffer_size_multiplier).astype(jnp.int32)
 
-def cell_dimensions(cell, cutoff):
-    """Compute the number of cells-per-side and total number of cells in a box."""
+def bin_dimensions(cell, cutoff):
+    """Compute the number of bins-per-side and total number of bins in a box."""
     # Considering cell is 3x3 array
     # Transform into reciprocal space to get the cell size whatever cell is
     face_dist = get_heights(cell)
-    cells_per_side = jnp.floor(face_dist / cutoff).astype(jnp.int32)
-    cell_size = jnp.where(cells_per_side != 0, cell / cells_per_side, cell)
-    cell_count = jnp.prod(cells_per_side)
-    return cell, cell_size, cells_per_side, cell_count.astype(jnp.int32)
+    bins_per_side = jnp.floor(face_dist / cutoff).astype(jnp.int32)
+    bin_size = jnp.where(bins_per_side != 0, cell / bins_per_side, cell)
+    bin_count = jnp.prod(bins_per_side)
+    return cell, bin_size, bins_per_side, bin_count.astype(jnp.int32)
 
     
-def count_cell_filling(position, cell, minimum_cell_size):
+def count_bin_filling(position, cell, minimum_bin_size):
     """
-    Counts the number of particles per-cell in a spatial partitioning scheme.
+    Counts the number of particles per-bin in a spatial partitioning scheme.
     """
-    cell, cell_size, cells_per_side, _ = cell_dimensions(cell, minimum_cell_size)
-    hash_multipliers = compute_hash_constants(cells_per_side)
-    particle_index = jnp.array(jnp.floor(jnp.dot(position, inverse(cell_size.T).T)), dtype=jnp.int32)
+    cell, bin_size, bins_per_side, _ = bin_dimensions(cell, minimum_bin_size)
+    hash_multipliers = compute_hash_constants(bins_per_side)
+    particle_index = jnp.array(jnp.floor(jnp.dot(position, inverse(bin_size.T).T)), dtype=jnp.int32)
     particle_hash = jnp.sum(particle_index * hash_multipliers, axis=1)
-    filling = jnp.zeros_like(particle_hash, dtype=jnp.int32) # jnp.zeros((cell_count,), dtype=jnp.int32)
+    filling = jnp.zeros_like(particle_hash, dtype=jnp.int32)
     filling = filling.at[particle_hash].add(1)
     return filling
 
-def compute_hash_constants(cells_per_side):
+def compute_hash_constants(bins_per_side):
+    """Compute the hash constants for a given number of bins per side."""
     one = jnp.array([1])
-    cells_per_side = jnp.concatenate((one, cells_per_side[:-1]), axis=0)
-    return jnp.array(jnp.cumprod(cells_per_side), dtype=jnp.int32)
+    bins_per_side = jnp.concatenate((one, bins_per_side[:-1]), axis=0)
+    return jnp.array(jnp.cumprod(bins_per_side), dtype=jnp.int32)
 
-def unflatten_cell_buffer(arr, cells_per_side):
-    cells_per_side = tuple(cells_per_side)
-    return jnp.reshape(arr, cells_per_side + (-1,) + arr.shape[1:])
+def unflatten_bin_buffer(arr, bins_per_side):
+    """Unflatten the bin buffer to get the bin_id."""
+    bins_per_side = tuple(bins_per_side)
+    return jnp.reshape(arr, bins_per_side + (-1,) + arr.shape[1:])
 
-def neighboring_cells(dimension):
+def neighboring_bins(dimension):
+  """Generate the indices of the neighboring bins of a given bin"""
   for dindex in np.ndindex(*([3] * dimension)):
     yield jnp.array(dindex) - 1
 
 
 
 def shift_array(arr, dindex):
+    """Shift an array by a given index. It is used for the bin's neighbor list."""
     dx, dy, dz = tuple(dindex) + (0,) * (3 - len(dindex))
     arr = cond(dx < 0,
                lambda x: jnp.concatenate((x[1:], x[:1])),
@@ -401,7 +413,6 @@ def shift_array(arr, dindex):
                arr)
 
     return arr
-
 
 # I had to include it for reallocate cell list, calling cell_list allocate not jitable
 def check_reallocation_cell_list(cell_list, allocate_fn, positions):
